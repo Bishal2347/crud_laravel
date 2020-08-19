9e7c95799a3a79cb4afee38da586b767781b8e5d
BIshal <47469643+Bishal2347@users.noreply.github.com>
2020-08-19 11:51:13 +0600

    Add files via upload

---
+++ storage/framework/views/0e7ac34016085c74a9a66a14c7474b631e0f2401.php
@@ -0,0 +1,54 @@
+
+ 
+<?php $__env->startSection('content'); ?>
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Check all Blogs</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-success" href="<?php echo e(route('blogs.create')); ?>"> Create new blogs</a>
+            </div>
+        </div>
+    </div>
+   
+    <?php if($message = Session::get('success')): ?>
+        <div class="alert alert-success">
+            <p><?php echo e($message); ?></p>
+        </div>
+    <?php endif; ?>
+   
+    <table class="table table-bordered">
+        <tr>
+            <th>No</th>
+            <th>Title</th>
+            <th>Description</th>
+            <th width="250px">Action</th>
+        </tr>
+        <?php $__currentLoopData = $blogs; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $blog): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+        <tr>
+            <td><?php echo e(++$i); ?></td>
+            <td><?php echo e($blog->title); ?></td>
+            <td><?php echo e($blog->description); ?></td>
+            <td>
+                <form action="<?php echo e(route('blogs.destroy',$blog->id)); ?>" method="POST">
+   
+                    <a class="btn btn-info" href="<?php echo e(route('blogs.show',$blog->id)); ?>">Show</a>
+    
+                    <a class="btn btn-primary" href="<?php echo e(route('blogs.edit',$blog->id)); ?>">Edit</a>
+   
+                    <?php echo csrf_field(); ?>
+                    <?php echo method_field('DELETE'); ?>
+      
+                    <button type="submit" class="btn btn-danger">Delete</button>
+                </form>
+            </td>
+        </tr>
+        <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+    </table>
+  
+    <?php echo $blogs->links(); ?>
+
+      
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('blogs.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\All Semister Study\8th Semister-2020\Software Engineering Web Application\Class Task\Make crud application using laravel framework\CrudApplication\resources\views/blogs/index.blade.php ENDPATH**/ ?>
---
+++ storage/framework/views/7d3ddb1f5cad154a25be6a3dfe3c277bd94dc319.php
@@ -0,0 +1,48 @@
+
+  
+<?php $__env->startSection('content'); ?>
+<div class="row">
+    <div class="col-lg-12 margin-tb">
+        <div class="pull-left">
+            <h2>Create New Blog</h2>
+        </div>
+        <div class="pull-right">
+            <a class="btn btn-primary" href="<?php echo e(route('blogs.index')); ?>"> Back</a>
+        </div>
+    </div>
+</div>
+   
+<?php if($errors->any()): ?>
+    <div class="alert alert-danger">
+        <strong>Warning!</strong> Please check your input code<br><br>
+        <ul>
+            <?php $__currentLoopData = $errors->all(); $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $error): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
+                <li><?php echo e($error); ?></li>
+            <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
+        </ul>
+    </div>
+<?php endif; ?>
+   
+<form action="<?php echo e(route('blogs.store')); ?>" method="POST">
+    <?php echo csrf_field(); ?>
+     <div class="row">
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Title:</strong>
+                <input type="text" name="title" class="form-control" placeholder="Title">
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12">
+            <div class="form-group">
+                <strong>Description:</strong>
+                <textarea class="form-control" style="height:280px" name="description" placeholder="Description"></textarea>
+            </div>
+        </div>
+        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                <button type="submit" class="btn btn-primary">Submit</button>
+        </div>
+    </div>
+   
+</form>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('blogs.layout', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\All Semister Study\8th Semister-2020\Software Engineering Web Application\Class Task\Make crud application using laravel framework\CrudApplication\resources\views/blogs/create.blade.php ENDPATH**/ ?>
